15267b57b06a1763d2578141c9e07ab91da46a88
piazin <ls4803326@gmail.com>
2023-03-23 19:35:52 -0300

    :heavy_plus_sign: build: remove venom, add wpp connect

--- src/app.ts
+++ src/app.ts
@@ -1,26 +1,25 @@
-import { create, Whatsapp, Message } from 'venom-bot';
+import { create, Whatsapp, Message } from '@wppconnect-team/wppconnect';
 import { stages } from './stages';
 import { StorageService } from './services/storage.service';
 import { config } from 'dotenv';
 
 config();
 
-create('suport-t.i-sl')
+create({ session: 'suport-ti' })
   .then((client) => start(client))
   .catch((err) => console.error('🚀 ~ file: app.ts:6 ~ err', err));
 
 function start(client: Whatsapp): void {
-  client.onMessage((message: Message) => {
+  client.onMessage(async (message: Message) => {
     if (message.isGroupMsg) return;
 
     const to = message.from;
-
     const storageService = new StorageService(to);
     var stage = storageService.getStage();
-
     var currentStage = stages[stage].stage;
 
+    const stageInstance = new currentStage(to);
     //@ts-ignore
-    new currentStage(to).execute({ to, client, message });
+    stageInstance.execute({ to, client, message });
   });
 }
